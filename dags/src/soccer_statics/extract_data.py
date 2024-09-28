337f6e125027d45ac20edebddf3ebf30634b4d91
danielcristancho <daniel.cristancho@klym.com>
2024-09-27 22:43:59 -0500

    Add new requirements

--- dags/src/soccer_statics/extract_data.py
+++ dags/src/soccer_statics/extract_data.py
@@ -2,13 +2,14 @@ from airflow.models import Variable
 import logging
 import requests
 import pandas as pd
+import json
 
 class Request_Data:
 
     def __init__(self):
         self.url = Variable.get("URL_SOCCER_STATICS")
-        self.query_api = Variable.get("QUERIE_API_SOCCER")
-        self.header = Variable.get("PASSWORD_HEADERS_API_SOCCER")
+        self.query_api = json.loads(Variable.get("QUERIE_API_SOCCER"))
+        self.header = json.loads(Variable.get("PASSWORD_HEADERS_API_SOCCER"))
         log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         logging.basicConfig(level=logging.INFO, format=log_fmt)
         self.__logger = logging.getLogger(__name__)
@@ -16,12 +17,12 @@ class Request_Data:
 
     def request_return(self):
 
-        data = pd.Dataframe([])
+        data = pd.DataFrame([])
         response = requests.get(self.url, headers=self.header, params=self.query_api)
         if response.status_code == 200:
             data = response.json()
             data = [item for item in data['response'] if item['fixture']['status']['short'] == 'FT']
-            data = pd.Dataframe(data)
+            data = pd.DataFrame(data)
 
         return data
 
--- dags/tab_soccer_statics.py
+++ dags/tab_soccer_statics.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from airflow.operators.python import PythonOperator
 from airflow import DAG
 from airflow.utils.dates import days_ago
@@ -11,21 +12,24 @@ from src.soccer_statics.dim_teams import Dim_Teams
 START_DATE = days_ago(1)
 
 
-def extract_data():
+def extract_data(**kwargs):
     try:
-        global data_extract
         pf = Request_Data()
         data_extract = pf.request_return()
-        print("****************** Success process update basics datamart finance")
+        kwargs['ti'].xcom_push(key="data_extract", value=data_extract.to_json())
+        print("****************** Success process extract data from api scores")
     except:
         print(
-            "****************** Unexpected error update basics datamart finance: ",
+            "****************** Unexpected error: ",
             sys.exc_info(),
         )
         raise
 
-def dim_teams():
+def dim_teams(**kwargs):
     try:
+        ti = kwargs["ti"]
+        data_extract = ti.xcom_pull(task_ids='extract_data', key='data_extract')
+        data_extract = pd.read_json(data_extract)
         data_teams = data_extract[['teams']]
         pf = Dim_Teams(data_teams=data_teams)
         pf.run()
@@ -60,7 +64,7 @@ extract_data = PythonOperator(
 )
 
 dim_teams = PythonOperator(
-    task_id="extract_data",
+    task_id="dim_teams",
     python_callable=dim_teams,
     dag=dag,
 )
