3dfd1f740c77e0519049aa5efc41d193edae00c4
danielcristancho <daniel.cristancho@klym.com>
2024-10-04 09:16:09 -0500

    Add new conf

--- dags/src/soccer_statics/extract_data.py
+++ dags/src/soccer_statics/extract_data.py
@@ -3,13 +3,13 @@ import logging
 import requests
 import pandas as pd
 import json
+import ast
 
 
 class Request_Data:
 
     def __init__(self):
         self.url = Variable.get("URL_SOCCER_STATICS")
-        self.query_api = json.loads(Variable.get("QUERIE_API_SOCCER"))
         self.header = json.loads(Variable.get("PASSWORD_HEADERS_API_SOCCER"))
         log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         logging.basicConfig(level=logging.INFO, format=log_fmt)
@@ -18,14 +18,23 @@ class Request_Data:
     def request_return(self):
 
         data = pd.DataFrame([])
-        response = requests.get(self.url, headers=self.header, params=self.query_api)
-        if response.status_code == 200:
-            data = response.json()
-            data = [
-                item
-                for item in data["response"]
-                if item["fixture"]["status"]["short"] == "FT"
-            ]
-            data = pd.DataFrame(data)
+        leagues = ast.literal_eval(Variable.get("LEAGUES_TO_CALL"))
+        year =  Variable.get("YEAR_TO_CALL")
+        for id_league in leagues:
+            query_api = {
+                "league": f"{id_league}",
+                "season": f"{year}"
+            }
+            print(query_api)
+            response = requests.get(self.url, headers=self.header, params=query_api)
+            if response.status_code == 200:
+                data_new = response.json()
+                data_new = [
+                    item
+                    for item in data_new["response"]
+                    if item["fixture"]["status"]["short"] == "FT"
+                ]
+                data_new = pd.DataFrame(data_new)
+                data = pd.concat([data_new,data], ignore_index=True)
 
         return data
